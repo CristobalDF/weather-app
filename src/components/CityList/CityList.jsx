4ccd7505a595979c990f3a6a1d50ed582cc61eb7
CristobalDF <cdelfierro@dt.gob.cl>
2022-02-18 13:37:57 -0300

    fixing

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,10 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios'
+import convert from 'convert-units'
+import { Alert } from '@material-ui/lab'
 import Grid from '@material-ui/core/Grid'
+import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import CityInfo from './../CityInfo'
 import Weather from './../Weather'
+
+const getCityCode = (city, countryCode) => `${city}-${countryCode}`
 
 //li: es un item en html que tiene el role "listitem" para efectos de TDD
 
@@ -43,50 +48,81 @@ const renderCityAndCountry = eventOnClickCity => (cityAndCountry, weather) => {
 const CityList = ({ cities, onClickCity }) => {
 
     const [allWeather, setAllWeather] = useState({})
+    const [error, setError] = useState(null)
 
     useEffect(() => {
 
-        const setWeather  = (city, country, countryCode) => {
-           const appid = "0443061d390902238e43d4cf886f060f"
-           const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${appid}`
-            axios
-            .get(url)
-            .then(response =>{
-                const {data} = response
-                const temperature = data.main.temp
-                const state = data.weather[0].main.toLowerCase()
+    // forma 1
+    //     const setWeather  = (city, country, countryCode) => {
+    //        const appid = "0443061d390902238e43d4cf886f060f"
+    //        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${appid}`
+    //         axios
+    //         .get(url)
+    //         .then(response =>{
+    //             const {data} = response
+    //             const temperature = Number(convert(data.main.temp).from("K").to("C").toFixed(1))
+    //             const state = data.weather[0].main.toLowerCase()
 
-                const propName = `${city}-${country}` //EJ: [Ciudad de México-México]
-                const propValue = { temperature, state} // EJ: temperature: 10, state: sunny
-                console.log('propValue: ', propValue);
-                console.log('propName: ', propName);
-                //Al hacer el destructuring de esta manera estamos desensamblar allWeather, 
-                /* 
-                allWeather primera pasada:
-                    [Buenos Aires-Argentina]: { temperature: 10, state: "sunny"},
-                allWeather segunda pasada:
-                    [Buenos Aires-Argentina]: { temperature: 10, state: "sunny"},
-                    [Santiago-Chile]: { temperature: 13, state: "sunny"},
-                */
-                setAllWeather( allWeather => ({...allWeather, [propName]: propValue }))
-        })
+    //             const propName = `${city}-${country}` //EJ: [Ciudad de México-México]
+    //             const propValue = { temperature, state} // EJ: temperature: 10, state: sunny
+
+    //             setAllWeather( allWeather => ({...allWeather, [propName]: propValue }))
+    //         })
+    //         .catch( error => {
+    //             if (error.response){ //Errores que nos responde el server
+    //                 setError("Ha ocurrido un error en el servidor del clima")
+    //             } else if (error.request){//Errores que suceden por no llegar al server
+    //                 setError("Server inaccesible")
+    //             } else { //Errores inprevistos
+    //                 setError("Error al cargar los datos")
+    //             }
+    //          })
+    // }
+    
+        const setWeather  = async (city, countryCode) => {
+        const appid = "0443061d390902238e43d4cf886f060f"
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${appid}`
+        try {
+            const response = await axios.get(url)
+
+            const {data} = response
+            const temperature = Number(convert(data.main.temp).from("K").to("C").toFixed(1))
+            const state = data.weather[0].main.toLowerCase()
+            const propName = getCityCode(city, countryCode)
+            const propValue = { temperature, state} // EJ: temperature: 10, state: sunny
+            setAllWeather( allWeather => ({...allWeather, [propName]: propValue }))
+        } catch (error) {
+            if (error.response){ //Errores que nos responde el server
+                setError("Ha ocurrido un error en el servidor del clima")
+            } else if (error.request){//Errores que suceden por no llegar al server
+                setError("Server inaccesible")
+            } else { //Errores inprevistos
+                setError("Error al cargar los datos")
+            }
+        }
     }
-        cities.forEach(({city, country, countryCode}) => {
-            setWeather(city, country, countryCode)
+        cities.forEach(({city, countryCode}) => {
+            setWeather(city, countryCode)
         });
         
     }, [cities])
 
-    //const weather = {temperature: 10, state: "sunny"}
     return (
-        <ul>
+        <>
             {
-                cities.map(cityAndCountry => renderCityAndCountry(onClickCity)(cityAndCountry, 
-                    allWeather[`${cityAndCountry.city}-${cityAndCountry.country}`]))
-            }
-        </ul>
+                error && <Alert onClose = {() => setError(null)} severity='error'>{error}</Alert>
+            }   
+            <List>
+                {
+                    cities.map(cityAndCountry => renderCityAndCountry(onClickCity)(cityAndCountry, 
+                        allWeather[getCityCode(cityAndCountry.city, cityAndCountry.countryCode)]))
+                }           
+            </List>
+        </>
+
     )
 }
+
 
 CityList.propTypes = {
     cities: PropTypes.arrayOf(
